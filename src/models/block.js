41f07287f00fbae78ffbbd8a53696bd1fdc446bd
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-04-04 15:47:24 +1000

    chore: add blocks

---
+++ src/models/block.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const blockSchema = new mongoose.Schema({
+  height: {
+    required: true,
+    type: Number,
+  },
+  version: {
+    required: true,
+    type: Number,
+  },
+  hash: {
+    required: true,
+    type: String,
+  },
+  merkle_root: {
+    required: true,
+    type: String,
+  },
+  previous_block_hash: {
+    required: true,
+    type: String,
+  },
+  timestamp: {
+    required: true,
+    type: Number,
+  },
+  validator: {
+    required: true,
+    type: String,
+  },
+  signature: {
+    required: true,
+    type: String,
+  },
+  txs: {
+    required: true,
+    type: Array,
+  },
+});
+
+module.exports = mongoose.model("Block", blockSchema);
