93475091047177ef01cb425402527540e791913c
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-04-16 09:46:35 +1000

    feat: add nodemon and version

--- js/src/express.js
+++ js/src/express.js
@@ -4,8 +4,14 @@ const app = express();
 const cors = require("cors");
 const rpc = require("./vm/rpc");
 
+const version = "1.0.0";
+
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ version });
+});
 
 app.use("/rpc", rpc);
 
