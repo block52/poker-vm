1fa41a25db9ab057616d06244b203d096d105cd1
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-05-09 14:41:39 +1000

    chore: update readmes, and contracts

--- contracts/test/Deposit.ts
+++ contracts/test/Deposit.ts
@@ -6,7 +6,7 @@ import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import hre from "hardhat";
 
-describe("Lock", function () {
+describe("Deposit", () => {
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
@@ -26,7 +26,7 @@ describe("Lock", function () {
     return { lock, unlockTime, lockedAmount, owner, otherAccount };
   }
 
-  describe("Deployment", function () {
+  describe("Deployment", () => {
     it("Should set the right unlockTime", async function () {
       const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
 
--- pvm/js/README.md
+++ pvm/js/README.md
@@ -1,6 +1,18 @@
 
 ```bash
-yarn start
+yarn express
+```
+
+## Test accounts
+
+#0 0xe05Af8f0689F0BcB7A90FA97B877b4CD14373e5F
+#1 0x5D2576A315F7c9e66E5803952bbE2bbe96A53C73
+
+### .env Example
+
+```text
+PORT=3000
+SEED=east monitor scheme fee right allow film what render stereo practice miss
 ```
 
 ## RPC Commands
@@ -9,9 +21,27 @@ yarn start
 
 Sends tokens on the layer 2 network.
 
+```json
+{
+    "method": "transfer",
+    "params": "0xe05Af8f0689F0BcB7A90FA97B877b4CD14373e5F",
+        "amount": 100
+    },
+    "id": 1,
+    "jsonrpc": "2.0"
+}
+```
+
 ### Join
 
 
+```json
+{
+
+}
+```
+
+### Exit
 
 ```json
 {
