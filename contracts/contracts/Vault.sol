54f0227cf7b572813cb15ff41f620ff73cb666c5
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-05-27 13:08:32 +1000

    chore: Remove Deposit contract and test file

--- contracts/contracts/Vault.sol
+++ contracts/contracts/Vault.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.24;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract Deposit {
+contract Vault {
     mapping(address => uint) public balances;
     mapping(address => bool) public validators;
 
