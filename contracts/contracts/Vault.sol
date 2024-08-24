d36feb06e6c16fcf07c76019ebd20cf01c9de389
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-08-24 15:51:11 +1000

    refactor: Add I4626 import to Vault contract

--- contracts/contracts/Vault.sol
+++ contracts/contracts/Vault.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.24;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { I4626 } from "./I4626.sol";
 
 contract Vault {
     mapping(address => uint) public balances;
