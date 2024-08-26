cc6d23a8268fabbbd444678b906df5ff140eb093
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-08-26 14:10:30 +1000

    refactor: Add bootnodes.json for local development and testing

--- pvm/js/src/schemas/transaction.js
+++ pvm/js/src/schemas/transaction.js
@@ -1,30 +1,35 @@
 const mongoose = require("mongoose");
 
-const transactionSchema = new mongoose.Schema({
-  account: {
-    required: true,
-    type: String,
+const transactionSchema = new mongoose.Schema(
+  {
+    account: {
+      required: true,
+      type: String,
+    },
+    nonce: {
+      required: true,
+      type: Number,
+    },
+    amount: {
+      required: true,
+      type: Number,
+    },
+    data: {
+      required: true,
+      type: String,
+    },
+    signature: {
+      required: true,
+      type: String,
+    },
+    timestamp: {
+      required: true,
+      type: Number,
+    },
   },
-  nonce: {
-    required: true,
-    type: Number,
-  },
-  amount: {
-    required: true,
-    type: Number,
-  },
-  data: {
-    required: true,
-    type: String,
-  },
-  signature: {
-    required: true,
-    type: String,
-  },
-  timestamp: {
-    required: true,
-    type: Number,
-  },
-});
+  {
+    timestamp: true,
+  }
+);
 
 module.exports = mongoose.model("Transaction", transactionSchema);
