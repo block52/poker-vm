5d61e320c737fda0f604ded1ac364ecc0d25fadb
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-09-11 15:57:33 +1000

    feat: Add Docker Compose file for MongoDB
    Add a new Docker Compose file to the explorer_api directory to set up a MongoDB container. The file includes the necessary environment variables for initializing the MongoDB database. This will allow for easier deployment and testing of the application.
    
    Also, update the package.json file to include new dependencies: axios and dotenv. These packages will be used for making HTTP requests and managing environment variables, respectively.
    
    Finally, update the index.js file to include new routes for retrieving blocks and block details. The /blocks route will be used to retrieve a list of all blocks, while the /block/:id route will retrieve details for a specific block using an HTTP POST request to the specified node URL.
    
    This commit addresses the need for a MongoDB container, adds necessary dependencies, and expands the functionality of the API.

--- explorer_api/src/index.js
+++ explorer_api/src/index.js
@@ -1,10 +1,43 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const axios = require("axios");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const node_url = process.env.NODE_ENV || "http://localhost:3000";
+const port = process.env.PORT || 3000;
 
 // Hello World GET route
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/block/:id", async (req, res) => {
+  const data = JSON.stringify({
+    method: "get_block",
+    params: [0],
+    id: 1,
+    jsonrpc: "2.0",
+    data: "",
+  });
+
+  const config = {
+    method: "post",
+    maxBodyLength: Infinity,
+    url: node_url,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: data,
+  };
+
+  const response = await axios.request(config);
+  res.send(response.data);
+});
+
+app.get("/blocks", (req, res) => {
+  res.send("Blocks route");
 });
 
 // Start the server
