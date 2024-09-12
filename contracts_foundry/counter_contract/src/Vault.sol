df3a937dd6a91acfb390cbb33cb4563485d1850a
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-09-12 18:34:11 +1000

    feat: Update Vault.sol and add package.json for contracts_foundry

--- contracts_foundry/counter_contract/src/Vault.sol
+++ contracts_foundry/counter_contract/src/Vault.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
-import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+// import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 // import { I4626 } from "@openzeppelin/contracts/token/ERC20/I4626.sol";
 
 contract Vault {
