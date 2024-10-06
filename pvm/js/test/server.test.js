45996b6922c9a74f76f2867d377c5a7303f56524
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-10-06 10:43:25 +1000

    Refactor package.json and yarn.lock

---
+++ pvm/js/test/server.test.js
@@ -0,0 +1,13 @@
+import { expect } from "chai";
+import { verify_signature } from "../src/crypto_utils";
+
+describe("Server", function () {
+  it("should process messages", function () {
+    // const server = new Server("0x");
+    // const message = {
+    //   method: "get_version",
+    // };
+    // const response = await server.processMessage(message);
+    // assert.equal(response, 1);
+  });
+});
