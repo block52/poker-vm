370916d0fa1937b2eeb7ba67e18141d91c1ec22d
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-10-16 16:32:24 +1000

    Refactor Account class to add createAccount and addBalance methods

--- pvm/js/src/models/account.mjs
+++ pvm/js/src/models/account.mjs
@@ -30,6 +30,38 @@ class Account {
   getModel() {
     return this.AccountModel;
   }
+
+  async createAccount(address) {
+    const query = { address };
+    const account = await this.AccountModel.findOne(query);
+
+    if (account) {
+      return account;
+    }
+
+    const new_account = new this.AccountModel({
+      address,
+      balance: 0,
+      nonce: 0,
+    });
+  }
+
+  async addBalance(address, amount) {
+    if (amount < 0) {
+      throw new Error("Amount must be positive");
+    }
+
+    let account = await this.getAccount(address);
+
+    if (!account) {
+      account = await this.createAccount(address);
+    }
+
+    account.balance += amount;
+    await account.save();
+
+    return account;
+  }
 }
 
 export default Account;
