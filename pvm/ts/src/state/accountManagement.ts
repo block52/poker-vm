54fe5b1918ac92e5ab8d50008b98f75020b7a6d6
Barry Earsman <barry@earsman.com>
2024-10-21 17:29:58 +1000

    feat: play transactions

--- pvm/ts/src/state/accountManagement.ts
+++ pvm/ts/src/state/accountManagement.ts
@@ -1,5 +1,6 @@
 import { Account } from "../models/account";
 import Accounts from "../schema/accounts";
+import { Transaction } from "../models/transaction";
 
 export class AccountManagement {
   constructor() {
@@ -7,7 +8,7 @@ export class AccountManagement {
   }
 
   async createAccount(address: string): Promise<Account> {
-    const account = new Account(address, 0);
+    const account = new Account(address, 0n);
     await Accounts.create(account.toJson());
     return account;
   }
@@ -23,14 +24,35 @@ export class AccountManagement {
   }
 
   // Helper functions
-  async getBalance(address: string): Promise<number> {
+  async getBalance(address: string): Promise<bigint> {
     const account = await this.getAccount(address);
     return account.balance;
   }
 
-  async incrementBalance(address: string, balance: number): Promise<void> {
+  async incrementBalance(address: string, balance: bigint): Promise<void> {
     await Accounts.updateOne({ address }, { $inc: { balance } });
   }
+
+  async decrementBalance(address: string, balance: bigint): Promise<void> {
+    await Accounts.updateOne({ address }, { $inc: { balance: -balance } });
+  }
+
+  async applyTransaction(tx: Transaction) {
+      // Deduct from sender
+      if (tx.from) {
+        this.decrementBalance(tx.from, tx.value);
+      }
+
+      // Add to recipient
+      this.incrementBalance(tx.to, tx.value);
+  }
+
+  async applyTransactions(txs: Transaction[]): Promise<void> {
+    for (const tx of txs) {
+      await this.applyTransaction(tx);
+    }
+  }
+
 }
 
 export default AccountManagement;
