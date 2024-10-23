a556ea3430a03810a759f9ee465c5fbc8a30baa9
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-10-23 12:18:14 +1000

    chore: Update update.sh script and add shutdown request
    This commit updates the update.sh script to include a new request to shutdown the application. The request is sent to the local server at http://localhost:3000/ with the necessary parameters. Additionally, the commit includes the command to stash any local changes and pull the latest changes from the remote repository.

--- update.sh
+++ update.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+curl --location 'http://localhost:3000/' \
+--header 'Content-Type: application/json' \
+--data '{
+    "method": "shutdown",
+    "params": ["username", "password"],
+    "id": 1,
+    "jsonrpc": "2.0",
+    "data": ""
+}'
+
 git stash
 git pull
 # git clone https://github.com/block52/poker-vm.git
