dbc61088b89b6bb34e7bf960a0627e99a63f3e62
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-10-23 12:15:38 +1000

    chore: Update install.sh and update.sh scripts

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo ufw allow 22
+sudo ufw allow 8000
+
+# install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
--- update.sh
+++ update.sh
@@ -3,6 +3,6 @@
 git stash
 git pull
 # git clone https://github.com/block52/poker-vm.git
-cd /pvm/ts
+cd pvm/ts
 yarn install
 yarn build
