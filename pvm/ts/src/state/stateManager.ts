371eecae296f6b56b5bfe58f6bbb3ed22f9e2255
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-10-25 15:04:20 +1000

    Refactor database connection logging

--- pvm/ts/src/state/stateManager.ts
+++ pvm/ts/src/state/stateManager.ts
@@ -3,11 +3,10 @@ import mongoose from "mongoose";
 export abstract class StateManager {
   constructor(
     readonly connectionString: string = "mongodb://localhost:27017/pvm"
-  ) {}
+  ) { }
 
   async connect() {
     try {
-      // const uri = process.env.MONGO_URI || "mongodb://localhost:27017/pvm";
       await mongoose.connect(this.connectionString);
       console.log("MongoDB connected");
     } catch (error) {
