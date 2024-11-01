241ac4a72e853044369dfe57a35f8420bf62b9ec
Barry Earsman <barry@earsman.com>
2024-11-01 12:32:33 +1000

    feat: game actions

--- game/src/components/poker/Game.tsx
+++ game/src/components/poker/Game.tsx
@@ -2,6 +2,23 @@ import { TexasHoldemState } from "@/types/game";
 
 import PlayerList from "./PlayerList";
 import CardSet from "./CardSet";
+import { GameActions } from "./GameActions";
+
+function handleCall() {
+    console.log("Call");
+}
+
+function handleRaise(amount: number) {
+    console.log(`Raise ${amount}`);
+}
+
+function handleCheck() {
+    console.log("Check");
+}
+
+function handleFold() {
+    console.log("Fold");
+}
 
 export default function Game({ state }: { state: TexasHoldemState }) {
     return (
@@ -9,6 +26,14 @@ export default function Game({ state }: { state: TexasHoldemState }) {
             <PlayerList players={state.players} />
             <CardSet name="community" cards={[...state.flop, state.turn, state.river]} />  
             <CardSet name="hole" cards={state.players[0].holeCards || []} />
+            <GameActions
+                onCall={() => handleCall()}
+                onRaise={(amount) => handleRaise(amount)}
+                onCheck={() => handleCheck()}
+                onFold={() => handleFold()}
+                minRaise={10}
+                maxRaise={1000}
+            />
         </div>
     );
 }
