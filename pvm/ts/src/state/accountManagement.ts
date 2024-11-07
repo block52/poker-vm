4dc2eb8ee6226cbba308feab3fd7c6f9bb1e1fef
Mitchell Westmoreland <mitch@dltx.io>
2024-11-07 16:40:44 +1000

    Prevent creation of duplicate accounts

--- pvm/ts/src/state/accountManagement.ts
+++ pvm/ts/src/state/accountManagement.ts
@@ -3,63 +3,67 @@ import Accounts from "../schema/accounts";
 import { Transaction } from "../models/transaction";
 
 export class AccountManagement {
-  constructor() {
+    constructor() {}
 
-  }
+    async createAccount(privateKey: string): Promise<Account> {
+        const account = Account.create(privateKey);
 
-  async createAccount(privateKey: string): Promise<Account> {
-    const account = Account.create(privateKey);
-    await Accounts.create(account.toDocument());
-    return account;
-  }
+        let accountAlreadyExists;
+        try {
+            await this.getAccount(account.address);
+            accountAlreadyExists = true;
+        } catch (e) {
+            accountAlreadyExists = false;
+        }
 
-  async getAccount(address: string): Promise<Account> {
-    const account = await Accounts.findOne({ address });
-
-    if (!account) {
-      throw new Error("Account not found");
+        if (accountAlreadyExists) {
+            throw new Error("Account already exists");
+        } else {
+            await Accounts.create(account.toDocument());
+            return account;
+        }
     }
 
-    return Account.fromDocument(account);
-  }
+    async getAccount(address: string): Promise<Account> {
+        const account = await Accounts.findOne({ address });
 
-  // Helper functions
-  async getBalance(address: string): Promise<bigint> {
-    const account = await this.getAccount(address);
+        if (!account) {
+            throw new Error("Account not found");
+        }
 
-    return account.balance;
-  }
-
-  async incrementBalance(address: string, balance: bigint): Promise<void> {
-    await Accounts.updateOne(
-      { address }, 
-      { $inc: { balance: balance.toString() } }
-    );
-  }
-
-  async decrementBalance(address: string, balance: bigint): Promise<void> {
-    await Accounts.updateOne(
-      { address }, 
-      { $inc: { balance: (-balance).toString() } }
-    );
-  }
-
-  async applyTransaction(tx: Transaction) {
-      // Deduct from sender
-      if (tx.from) {
-        await this.decrementBalance(tx.from, tx.value);
-      }
-
-      // Add to recipient
-      await this.incrementBalance(tx.to, tx.value);
-  }
-
-  async applyTransactions(txs: Transaction[]): Promise<void> {
-    for (const tx of txs) {
-      await this.applyTransaction(tx);
+        return Account.fromDocument(account);
     }
-  }
 
+    // Helper functions
+    async getBalance(address: string): Promise<bigint> {
+        const account = await this.getAccount(address);
+
+        return account.balance;
+    }
+
+    async incrementBalance(address: string, balance: bigint): Promise<void> {
+        await Accounts.updateOne({ address }, { $inc: { balance: balance.toString() } });
+    }
+
+    async decrementBalance(address: string, balance: bigint): Promise<void> {
+        await Accounts.updateOne({ address }, { $inc: { balance: (-balance).toString() } });
+    }
+
+    async applyTransaction(tx: Transaction) {
+        // Deduct from sender
+        if (tx.from) {
+            await this.decrementBalance(tx.from, tx.value);
+        }
+
+        // Add to recipient
+        await this.incrementBalance(tx.to, tx.value);
+    }
+
+    async applyTransactions(txs: Transaction[]): Promise<void> {
+        for (const tx of txs) {
+            await this.applyTransaction(tx);
+        }
+    }
 }
 
 export default AccountManagement;
