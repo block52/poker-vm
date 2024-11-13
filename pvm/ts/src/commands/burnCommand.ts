e2871d0aea0812a10c4fd1d827b8a09e8e546bad
Mitchell Westmoreland <mitch@dltx.io>
2024-11-13 15:51:27 +1000

    Make burn command work (not finished)

--- pvm/ts/src/commands/burnCommand.ts
+++ pvm/ts/src/commands/burnCommand.ts
@@ -1,73 +1,99 @@
-import { ethers, JsonRpcProvider } from "ethers";
+import { ethers, JsonRpcProvider, Contract, InterfaceAbi, ZeroAddress } from "ethers";
 import { getMempoolInstance } from "../core/mempool";
 import { Transaction } from "../models";
 import accounts from "../schema/accounts";
 import { signResult } from "./abstractSignedCommand";
 import { ISignedCommand, ISignedResponse } from "./interfaces";
 import { RandomCommand } from "./randomCommand";
+import { NativeToken } from "../models/nativeToken";
 
 export class BurnCommand implements ISignedCommand<Transaction> {
-    private readonly publicKey: string;
-    public readonly BridgeAddress = "0x0B6052D3951b001E4884eD93a6030f92B1d76cf0";
     private readonly randomCommand: RandomCommand;
+    private readonly bridge: Contract;
+    private readonly provider: JsonRpcProvider;
     private readonly signer: ethers.Wallet;
+    private readonly burnFromWallet: ethers.Wallet;
+    private readonly underlyingAssetAbi: InterfaceAbi;
 
-    constructor(readonly receiver: string, readonly amount: bigint, private readonly privateKey: string) {
+    constructor(readonly burnFrom: string, readonly amount: bigint, readonly bridgeTo: string, private readonly privateKey: string) {
+        if (!burnFrom) {
+            throw new Error("Private key to burn from must be provided");
+        }
+        try {
+            this.burnFromWallet = new ethers.Wallet(burnFrom);
+        } catch {
+            throw new Error("Invalid private key provided for account to burn from");
+        }
+
         if (amount <= 0) {
             throw new Error("Amount must be greater than 0");
         }
 
-        if (!receiver) {
-            throw new Error("Receiver must be provided");
+        if (!ethers.isAddress(bridgeTo)) {
+            throw new Error("Address to mint to must be provided");
         }
 
         if (!privateKey) {
             throw new Error("Private key must be provided");
         }
 
-        this.receiver = receiver;
         this.amount = amount;
+        this.bridgeTo = bridgeTo;
         this.privateKey = privateKey;
         this.randomCommand = new RandomCommand(32, "", this.privateKey);
         this.signer = new ethers.Wallet(privateKey);
-        this.publicKey = this.signer.address;
+
+        const bridgeAbi = ["function deposits(uint256) view returns (tuple(address account, uint256 amount))", "function underlying() view returns (address)"];
+        this.underlyingAssetAbi = ["function decimals() view returns (uint8)"];
+
+        const baseRPCUrl = process.env.RPC_URL;
+        this.provider = new JsonRpcProvider(baseRPCUrl, undefined, {
+            staticNetwork: true
+        });
+        this.bridge = new ethers.Contract(process.env.BRIDGE_CONTRACT_ADDRESS ?? ZeroAddress, bridgeAbi, this.provider);
     }
 
     public async execute(): Promise<ISignedResponse<Transaction>> {
-        // Minting logic
-        // Check tx hash is in the staking mainnet contract and has not be validated
-        // If we're a validator, we can mint
-        // Check the DB for the tx hash
-        // If it's not in the DB, mint
-        const account = await accounts.findOne({ address: this.receiver });
-        if (account) {
-            // return signResult(Transaction.fromDocument(existingTx), this.privateKey);
-            // throw new Exception();
+        const account = await accounts.findOne({ address: this.burnFromWallet.address });
+        if (!account) {
+            throw new Error("Burn from account not found");
         }
 
-        if (this.amount > Number(account?.balance)) {
-            // throw ...
+        if (this.amount > account.balance) {
+            throw new Error("Burn from account has insufficient funds");
         }
 
-        const abi = ["function withdraw(uint256 amount, address to, bytes32 nonce, bytes calldata signature)"];
+        const underlyingAssetAddress = await this.bridge.underlying();
+        const underlyingAsset = new ethers.Contract(underlyingAssetAddress, this.underlyingAssetAbi, this.provider);
+        const underlyingAssetDecimals = await underlyingAsset.decimals();
+        const amountToMint = NativeToken.convertToDecimals(this.amount, underlyingAssetDecimals);
+
         const nonce = await this.randomCommand.execute();
 
-        // TODO: Move to constructor
-        const baseRPCUrl = process.env.RPC_URL;
-        const provider = new JsonRpcProvider(baseRPCUrl, undefined, {
-            staticNetwork: true
-        });
+        console.log(`Address to burn from: ${this.burnFromWallet.address}`);
+        console.log(`Amount to burn: ${this.amount}`);
+        console.log(`Amount to mint: ${amountToMint}`);
+        console.log(`Address to bridge to: ${this.bridgeTo}`);
+        console.log(`Nonce: 0x${nonce.data.toString("hex")}`);
 
-        // Move to base class
-        const _signer = new ethers.Wallet(this.privateKey, provider);
-        const bridge = new ethers.Contract(this.BridgeAddress, abi, _signer);
+        const hash = ethers.solidityPackedKeccak256(["address", "uint256", "bytes"], [this.bridgeTo, amountToMint, nonce.data]);
+        console.log(`Hash: ${hash}`);
+        const signature = await this.signer.signMessage(ethers.getBytes(hash));
+        console.log(`Signed hash: ${signature}`);
 
-        const tx = await bridge.withdraw(this.amount, this.receiver, nonce.toString());
-        const burnTx: Transaction = Transaction.create(this.BridgeAddress, this.publicKey, this.amount, this.privateKey);
+        const bridgeAuthorisation = {
+            amount: amountToMint,
+            to: this.bridgeTo,
+            nonce: nonce,
+            signature: signature
+        };
+
+        const burnTx: Transaction = Transaction.create(null, this.burnFromWallet.address, this.amount, this.privateKey);
 
         // Send to mempool
         const mempoolInstance = getMempoolInstance();
         await mempoolInstance.add(burnTx);
+
         return signResult(burnTx, this.privateKey);
     }
 }
