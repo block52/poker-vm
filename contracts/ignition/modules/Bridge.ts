da44a893fde8917f2d4d809a83b827621e77636b
Mitchell Westmoreland <mitch@dltx.io>
2024-11-14 15:15:19 +1000

    Clean up contract

--- contracts/ignition/modules/Bridge.ts
+++ contracts/ignition/modules/Bridge.ts
@@ -3,9 +3,8 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 const BridgeModule = buildModule("BridgeModule", m => {
     const usdc = "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913";
     const vault = "0x961106278AD74432a0e7e532dBed2F2906725544";
-    const lock_time = 60;
 
-    const bridge = m.contract("Bridge", [usdc, vault, lock_time]);
+    const bridge = m.contract("Bridge", [usdc, vault]);
 
     return { bridge };
 });
