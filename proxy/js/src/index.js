a5d974732da182b62fbb91204752cc022a0ccb60
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-11-27 14:32:11 +1000

    feat: add development script and enhance game response structure in API

--- proxy/js/src/index.js
+++ proxy/js/src/index.js
@@ -21,10 +21,31 @@ app.get("/games", (req, res) => {
     const id1 = ethers.ZeroAddress;
     const id2 = ethers.ZeroAddress;
 
+    const min = 50; // ethers.utils.parseEther("50");
+    const max = 200; //ethers.utils.parseEther("200");
+
     const response = [
-        { id: id1, type: "No Limit Texas Holdem" },
-        { id: id2, type: "No Limit Texas Holdem" }
+        { id: id1, type: "No Limit Texas Holdem", max_players: 9, min, max },
+        { id: id2, type: "No Limit Texas Holdem", max_players: 6, min, max }
     ];
+
+    res.send(response);
+});
+
+app.post("/join", (req, res) => {
+    const response = {
+        id: 1,
+        balance: ethers.utils.formatEther(req.body.balance)
+    };
+    res.send(response);
+});
+
+// Deposit to the layer 2
+app.post("/deposit", (req, res) => {
+    const response = {
+        id: 1,
+        balance: ethers.utils.formatEther(req.body.balance)
+    };
     res.send(response);
 });
 
