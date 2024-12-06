89c6c35c7a91ca732da76b05dc58579d1eae089b
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-12-06 17:28:10 +1000

    feat: add router parameter to Bridge contract and update package.json to include publish script

--- contracts/ignition/modules/Bridge.ts
+++ contracts/ignition/modules/Bridge.ts
@@ -1,10 +1,12 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+import { ethers } from "hardhat";
 
 const BridgeModule = buildModule("BridgeModule", m => {
     const usdc = "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913";
     const vault = "0x961106278AD74432a0e7e532dBed2F2906725544";
+    const router = ethers.ZeroAddress;
 
-    const bridge = m.contract("Bridge", [usdc, vault]);
+    const bridge = m.contract("Bridge", [usdc, vault, router]);
 
     return { bridge };
 });
