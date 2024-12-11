6a631c019c15df8f3de3b3caccc02ee4c1383da5
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-12-12 08:06:15 +1000

    chore: add legacy plugin for Vite and update dependencies

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,10 +1,16 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
+import legacy from "@vitejs/plugin-legacy";
 import { defineConfig } from "vite";
 
 export default defineConfig({
     root: "./public/", // This points to the project root directory
-    plugins: [react()], // Vite React plugin
+    plugins: [
+        react(),
+        legacy({
+            targets: ["defaults", "not IE 11"]
+        })
+    ], // Vite React plugin
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src") // Alias for `src` directory
