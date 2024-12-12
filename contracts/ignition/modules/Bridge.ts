f2cf3604f11d9ee40ecc82253fcf116ba69a31b9
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-12-12 14:26:46 +1000

    feat: update Bridge module to use dynamic addresses based on network

--- contracts/ignition/modules/Bridge.ts
+++ contracts/ignition/modules/Bridge.ts
@@ -1,10 +1,15 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-import { ethers } from "hardhat";
 
 const BridgeModule = buildModule("BridgeModule", m => {
-    const usdc = "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913";
-    const vault = "0x961106278AD74432a0e7e532dBed2F2906725544";
-    const router = "0x2626664c2603336E57B271c5C0b26F421741e481"; // base v3 Router 2 // ethers.ZeroAddress;
+    let usdc = "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913";
+    let vault = "0x961106278AD74432a0e7e532dBed2F2906725544";
+    let router = "0x2626664c2603336E57B271c5C0b26F421741e481"; // base v3 Router 2 // ethers.ZeroAddress;
+
+    if (m.network.name === "mainnet") {
+        usdc = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+        vault = "";
+        router = "0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45";
+    }
 
     const bridge = m.contract("Bridge", [usdc, vault, router]);
 
