68854900430e1b9c87c9187b3b2aceb4ab669572
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2025-01-26 07:14:47 +1000

    refactor: remove sensitive MongoDB URI from db.js.  Fixes #125

--- proxy/js/src/db.js
+++ proxy/js/src/db.js
@@ -1,15 +1,11 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const FALLBACK_URI = "mongodb+srv://node1:7e0Kc3EIU4sl2651@db-mongodb-syd1-41573-30ceb254.mongo.ondigitalocean.com/pvm?tls=true&authSource=admin&replicaSet=db-mongodb-syd1-41573";
+const FALLBACK_URI = "";
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI || FALLBACK_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-
+        const conn = await mongoose.connect(process.env.MONGO_URI || FALLBACK_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
