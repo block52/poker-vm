8cb4c44681129173301f20c4b24b6c7deae1e83a
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2025-01-29 09:44:10 +1000

    v1.0.40

--- sdk/src/types/rpc.ts
+++ sdk/src/types/rpc.ts
@@ -25,16 +25,17 @@ export enum RPCMethods {
     GET_ACCOUNT = "get_account",
     GET_BALANCE = "get_balance",
     GET_BLOCK = "get_block",
-    GET_BLOCKS = "get_blocks",
     GET_BLOCK_BY_HASH = "get_block_by_hash",
     GET_BLOCK_HEIGHT = "get_block_height",
+    GET_BLOCKS = "get_blocks",
     GET_CLIENT = "get_client",
     GET_CONTRACT_SCHEMA = "get_contract_schema",
+    GET_GAME_STATE = "get_game_state",
     GET_LAST_BLOCK = "get_last_block",
     GET_MEMPOOL = "get_mempool",
     GET_NODES = "get_nodes",
+    GET_SHARED_SECRET = "get_shared_secret",
     GET_TRANSACTIONS = "get_transactions",
-    GET_GAME_STATE = "get_game_state",
     MINE = "mine",
     MINED_BLOCK_HASH = "mined_block_hash",
     MINT = "mint",
@@ -51,22 +52,23 @@ export type RPCRequestParams = {
     [RPCMethods.CREATE_CONTRACT_SCHEMA]: [string, string, any]; // [category, name, schema]
     [RPCMethods.GET_ACCOUNT]: [string]; // [address]
     [RPCMethods.GET_BALANCE]: [string]; // [address]
-    [RPCMethods.GET_BLOCK]: [string]; // [index]
-    [RPCMethods.GET_BLOCKS]: [string]; // [count]
     [RPCMethods.GET_BLOCK_BY_HASH]: [string]; // [hash]
     [RPCMethods.GET_BLOCK_HEIGHT]: []; // No parameters
+    [RPCMethods.GET_BLOCK]: [string]; // [index]
+    [RPCMethods.GET_BLOCKS]: [string]; // [count]
     [RPCMethods.GET_CLIENT]: []; // No parameters
     [RPCMethods.GET_CONTRACT_SCHEMA]: [string]; // [hash]
+    [RPCMethods.GET_GAME_STATE]: [string]; // [address]
     [RPCMethods.GET_LAST_BLOCK]: []; // No parameters
     [RPCMethods.GET_MEMPOOL]: []; // No parameters
     [RPCMethods.GET_NODES]: []; // No parameters
+    [RPCMethods.GET_SHARED_SECRET]: [string]; // [publicKey]
     [RPCMethods.GET_TRANSACTIONS]: [string]; // [count]
-    [RPCMethods.GET_GAME_STATE]: [string]; // [address]
     [RPCMethods.MINE]: []; // No parameters
-    [RPCMethods.MINT]: [string]; // [depositIndex]
     [RPCMethods.MINED_BLOCK_HASH]: [string, string]; // [blockHash, nodeUrl]
+    [RPCMethods.MINT]: [string]; // [depositIndex]
+    [RPCMethods.SHUTDOWN]: [string, string]; // [username, password]
     [RPCMethods.START]: []; // No parameters
     [RPCMethods.STOP]: []; // No parameters
     [RPCMethods.TRANSFER]: [string, string, string, string | null]; // [from, to, amount, data]
-    [RPCMethods.SHUTDOWN]: [string, string]; // [username, password]
 };
