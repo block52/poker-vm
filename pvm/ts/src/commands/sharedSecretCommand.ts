7d27a2abf21575036ad2733306eb078443bc2691
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2025-01-29 09:33:13 +1000

    feat: implement shared secret computation in SharedSecretCommand

--- pvm/ts/src/commands/sharedSecretCommand.ts
+++ pvm/ts/src/commands/sharedSecretCommand.ts
@@ -1,22 +1,23 @@
 import { ISignedCommand, ISignedResponse } from "./interfaces";
+import { SigningKey } from "ethers";
 
 export class SharedSecretCommand implements ISignedCommand<string> {
     constructor(
         private readonly publicKey: string,
         private readonly privateKey: string
-    ) {}
+    ) {
+        this.publicKey = publicKey.startsWith("0x") ? publicKey.slice(2) : publicKey;
+        this.privateKey = privateKey.startsWith("0x") ? privateKey.slice(2) : privateKey;
+    }
 
     public async execute(): Promise<ISignedResponse<string>> {
-        // sign1 = new SigningKey(id("some-secret-1"))
-        // sign2 = new SigningKey(id("some-secret-2"))
 
-        // // Notice that privA.computeSharedSecret(pubB)...
-        // sign1.computeSharedSecret(sign2.publicKey)
-        // // '0x04b5bc2a5428042331a4c70da8f090d5552bdb35bc08a00ea8ed0a9b6d8737b8b7ea016b268d7cb9f02e11736b82b129ea3f37a8fdc6a7b0e9f5cdde4105ceb0de'
+        const signingKey = new SigningKey(this.privateKey);
+        const sharedSecret = signingKey.computeSharedSecret(this.publicKey);
 
-        // // ...is equal to privB.computeSharedSecret(pubA).
-        // sign2.computeSharedSecret(sign1.publicKey)
-
-        throw new Error("Method not implemented.");
+        return {
+            response: sharedSecret,
+            signature: signingKey.signDigest(sharedSecret)
+        };
     }
 }
