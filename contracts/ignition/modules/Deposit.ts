cd807bf3003c803d1087069966f39fcd508df276
tommycp2 <thomasmillerprivate@gmail.com>
2025-02-05 14:46:24 +1000

    redeploy Bridge and Deposit with new update bridge contract function

--- contracts/ignition/modules/Deposit.ts
+++ contracts/ignition/modules/Deposit.ts
@@ -1,13 +1,13 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const USDCTOKEN = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";    // USDC on Ethereum
-const DEPLOYED_BRIDGE = "0xBf1e380f3D1AE31854764081C488EaDA9F4CB195";   // BridgeModule#Bridge on Ethereum
+const DEPLOYED_BRIDGE = "0x092eEA7cE31C187Ff2DC26d0C250B011AEC1a97d";   // BridgeModule#Bridge on Ethereum
 
-const DepositTestModule = buildModule("DepositTestModule", (m) => {
+const DepositModule = buildModule("DepositTestModule", (m) => {
     // Deploy new version of Deposit contract with token and bridge addresses
     const deposit = m.contract("Deposit", [USDCTOKEN, DEPLOYED_BRIDGE]);
 
     return { deposit };
 });
 
-export default DepositTestModule; 
+export default DepositModule; 
