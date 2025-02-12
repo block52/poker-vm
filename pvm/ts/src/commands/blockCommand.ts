166a2f64f909cb055bf3f9f3a10a719120701120
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2025-02-12 14:19:07 +1000

    feat: Enhance BlockCommand to retrieve and include transactions by block hash

--- pvm/ts/src/commands/blockCommand.ts
+++ pvm/ts/src/commands/blockCommand.ts
@@ -1,5 +1,6 @@
-import { Block } from "../models";
+import { Block, Transaction } from "../models";
 import { BlockchainManagement, getBlockchainInstance } from "../state/blockchainManagement";
+import { getTransactionInstance, TransactionManagement } from "../state/transactionManagement";
 import { signResult } from "./abstractSignedCommand";
 import { ISignedCommand, ISignedResponse } from "./interfaces";
 
@@ -10,10 +11,12 @@ export type BlockCommandParams = {
 
 export class BlockCommand implements ISignedCommand<Block> {
   private readonly blockchainManagement: BlockchainManagement;
+  private readonly transactionManagement: TransactionManagement;
   private readonly params: BlockCommandParams;
 
   constructor(params: BlockCommandParams, private readonly privateKey: string) {
     this.blockchainManagement = getBlockchainInstance();
+    this.transactionManagement = getTransactionInstance();
     this.params = params;
   }
 
@@ -21,6 +24,10 @@ export class BlockCommand implements ISignedCommand<Block> {
     // TODO: REGEX to validate hash
     if (this.params.hash) {
       const block: Block = await this.blockchainManagement.getBlockByHash(this.params.hash);
+
+      const transactions = await this.getTransactions(block.hash);
+      block.transactions.push(...transactions);
+
       return signResult(block, this.privateKey);
     }
 
@@ -34,9 +41,17 @@ export class BlockCommand implements ISignedCommand<Block> {
 
     if (this.params.index) {
       const block: Block = await this.blockchainManagement.getBlock(Number(this.params.index));
+
+      const transactions = await this.getTransactions(block.hash);
+      block.transactions.push(...transactions);
+      
       return signResult(block, this.privateKey);
     }
 
     return signResult(await this.blockchainManagement.getLastBlock(), this.privateKey);
+  }
+
+  private async getTransactions(hash: string): Promise<Transaction[]> {
+    return await this.transactionManagement.getTransactions(hash);
   }
 }
