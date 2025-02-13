9156c76d7b3d06d6ad76b9e34d22e082552dd35b
tommycp2 <thomasmillerprivate@gmail.com>
2025-02-13 14:19:56 +1000

    refactor: Update routing for Latest Blocks page

--- explorer/src/App.tsx
+++ explorer/src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "./components/theme/theme-provider";
 import { Navbar } from "./components/ui/navbar";
-import BlocksPage from "./components/pages/BlocksPage";
 import MempoolPage from "./components/pages/MempoolPage";
 import TransactionsPage from "./components/pages/TransactionsPage";
 import BlockPage from "./components/pages/BlockPage";
@@ -15,8 +14,8 @@ function App() {
                     <Navbar />
                     <main className="p-4">
                         <Routes>
-                            <Route path="/" element={<BlocksPage />} />
-                            <Route path="/blocks" element={<BlocksPage />} />
+                            <Route path="/" element={<LatestBlocksPage />} />
+                            <Route path="/blocks" element={<LatestBlocksPage />} />
                             <Route path="/latestblocks" element={<LatestBlocksPage />} />
                             <Route path="/block/:hash" element={<BlockPage />} />
                             <Route path="/mempool" element={<MempoolPage />} />
