f936255566cb27f4b66f2bf8adf2e3d6bbfbd1de
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2025-02-14 10:31:31 +1000

    fix: Correct variable usage in createProvider function to ensure proper node URL is used

--- pvm/ts/src/core/provider.ts
+++ pvm/ts/src/core/provider.ts
@@ -2,5 +2,5 @@ import { ethers } from "ethers";
 
 export const createProvider = (nodeUrl: string): ethers.JsonRpcProvider => {
     const _nodeUrl = nodeUrl || process.env.RPC_URL;
-    return new ethers.JsonRpcProvider(nodeUrl);
+    return new ethers.JsonRpcProvider(_nodeUrl);
 }
