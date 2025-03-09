83fae33175bf57dd32c86182d301dd7b8a079be1
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2025-03-09 10:58:00 +1000

    feat: implement preflop restriction in SmallBlindAction to enforce betting rules

--- pvm/ts/src/engine/actions/smallBlindAction.ts
+++ pvm/ts/src/engine/actions/smallBlindAction.ts
@@ -1,12 +1,19 @@
-import { PlayerActionType } from "@bitcoinbrisbane/block52";
+import { PlayerActionType, TexasHoldemRound } from "@bitcoinbrisbane/block52";
 import { Player } from "../../models/game";
 import BaseAction from "./baseAction";
-import { Range } from "../types";
+import { IAction, Range } from "../types";
 
-class SmallBlindAction extends BaseAction {
+class SmallBlindAction extends BaseAction implements IAction {
     get type(): PlayerActionType { return PlayerActionType.SMALL_BLIND }
 
     verify(_player: Player) : Range {
+        super.verify(_player);
+
+        // Can only bet the small blind amount when preflop
+        if (this.game.currentRound !== TexasHoldemRound.PREFLOP) {
+            throw new Error("Can only bet small blind amount when preflop.");
+        }
+
         return { minAmount: this.game.smallBlind, maxAmount: this.game.smallBlind };
     }
 
