1187575fdd75704f010dac8366777e09179cc3d0
tommycp2 <thomasmillerprivate@gmail.com>
2025-03-14 13:13:51 +1000

    feat: add useUserBySeat hook for fetching user data by address and seat, including loading state and error handling

---
+++ ui/src/hooks/useUserBySeat.ts
@@ -0,0 +1,51 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+interface UseUserSeatResult {
+    data: any;
+    isLoading: boolean;
+    error: Error | null;
+    refetch: () => Promise<void>;
+}
+
+const useUserBySeat = (address: string, seat: number): UseUserSeatResult => {
+    const [userData, setUserData] = useState<any>();
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState<Error | null>(null);
+
+    const fetchType = async () => {
+        if (!address) return;
+
+        setIsLoading(true);
+        setError(null);
+
+        try {
+            const mock_url = "https://orca-app-k9l4d.ondigitalocean.app";
+            const url = process.env.REACT_APP_PROXY_URL || "https://proxy.block52.xyz";
+            const response = await axios.get(`${mock_url}/table/${address}/player/${seat}`);
+
+            if (response.status !== 200) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            // console.log(response.data)
+            setUserData(response.data);
+        } catch (err) {
+            setError(err instanceof Error ? err : new Error("An error occurred"));
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchType();
+    }, [address, seat]);
+
+    return {
+        data: userData,
+        isLoading,
+        error,
+        refetch: fetchType // Correctly assign the fetch function
+    };
+};
+
+export default useUserBySeat;
