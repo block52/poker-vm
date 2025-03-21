ce987d75eb088fe915135338b66b53fbf8e1525a
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2025-03-21 11:00:28 +1000

    Implement IAction interface in CheckAction and add method to retrieve the largest bet in the current round

--- pvm/ts/src/engine/actions/checkAction.ts
+++ pvm/ts/src/engine/actions/checkAction.ts
@@ -1,14 +1,31 @@
 import { PlayerActionType } from "@bitcoinbrisbane/block52";
 import { Player } from "../../models/game";
 import BaseAction from "./baseAction";
-import { Range } from "../types";
+import { IAction, Range } from "../types";
 
-class CheckAction extends BaseAction {
+class CheckAction extends BaseAction implements IAction {
     get type(): PlayerActionType { return PlayerActionType.CHECK }
 
     verify(player: Player): Range | undefined {
+
+        // Check if s
+
         super.verify(player);
         return undefined
+    }
+
+    // Get the largest bet in the current round
+    private getLargestBet(): bigint {
+        let amount = 0n;
+        const roundBets = this.game.getBets(this.game.currentRound);
+
+        roundBets.forEach((bet) => {
+            if (bet > amount) {
+                amount = bet;
+            }
+        });
+
+        return amount;
     }
 }
 
