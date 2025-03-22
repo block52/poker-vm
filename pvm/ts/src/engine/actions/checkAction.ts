f478ef29494eb0b05305c8d3164523629aab034f
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2025-03-22 12:16:10 +1000

    feat: enhance check action verification for preflop round and add sum bets calculation

--- pvm/ts/src/engine/actions/checkAction.ts
+++ pvm/ts/src/engine/actions/checkAction.ts
@@ -1,15 +1,28 @@
-import { PlayerActionType } from "@bitcoinbrisbane/block52";
+import { PlayerActionType, TexasHoldemRound } from "@bitcoinbrisbane/block52";
 import { Player } from "../../models/player";
 import BaseAction from "./baseAction";
-import { IAction, Range } from "../types";
+import { IAction, Range, Turn } from "../types";
 
 class CheckAction extends BaseAction implements IAction {
     get type(): PlayerActionType { return PlayerActionType.CHECK }
 
     verify(player: Player): Range | undefined {
-        // Can never check if you havent matched the largest bet of the round
+
+        if (this.game.currentRound === TexasHoldemRound.PREFLOP) {
+            const preflopRoundBets = this.game.getBets();
+
+            if (preflopRoundBets.size === 0) {
+                throw new Error("Cannot check in the preflop round when there's been no action.");
+            }
+        }
+
+        // Can never check if you haven't matched the largest bet of the round
         const largestBet = this.getLargestBet();
-        // const playerBet = player.
+        const sumBets = this.getSumBets(player.address);
+
+        if (largestBet > sumBets) {
+            throw new Error("Player must match the largest bet to check.");
+        }
 
         super.verify(player);
         return undefined
@@ -27,6 +40,19 @@ class CheckAction extends BaseAction implements IAction {
         });
 
         return amount;
+    }
+
+    private getSumBets(playerId: string): bigint {
+        let totalBet = 0n;
+
+        const roundBets = this.game.getBets(this.game.currentRound);
+
+        // If the player made a bet in this round, add it to the total
+        if (roundBets.has(playerId)) {
+            totalBet += roundBets.get(playerId) || 0n;
+        }
+
+        return totalBet;
     }
 }
 
