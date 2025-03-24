e62cb18037607d8b13e111c4615ca2df3ef8bfc2
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2025-03-24 20:00:41 +1000

    fix: update mnemonic format in deck tests to include brackets and remove exclusive test flag

--- pvm/ts/src/models/deck.test.ts
+++ pvm/ts/src/models/deck.test.ts
@@ -26,7 +26,7 @@ describe("Deck", () => {
         });
 
         it("should serialize to string", () => {
-            const mnemonic = "AC-2C-3C-4C-5C-6C-7C-8C-9C-10C-JC-QC-KC-" +
+            const mnemonic = "[AC]-2C-3C-4C-5C-6C-7C-8C-9C-10C-JC-QC-KC-" +
                 "AD-2D-3D-4D-5D-6D-7D-8D-9D-10D-JD-QD-KD-" +
                 "AH-2H-3H-4H-5H-6H-7H-8H-9H-10H-JH-QH-KH-" +
                 "AS-2S-3S-4S-5S-6S-7S-8S-9S-10S-JS-QS-KS";
@@ -74,7 +74,7 @@ describe("Deck", () => {
         });
     });
 
-    describe.only("getNext and deal", () => {
+    describe("getNext and deal", () => {
         it("should draw next card correctly", () => {
             const card = deck.getNext();
             expect(card).toBeDefined();
