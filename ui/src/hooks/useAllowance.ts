18f1a0f0b36e8a80688021656cf04275c4b7135f
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2025-04-10 08:24:00 +1000

    chore: add constants

--- ui/src/hooks/useAllowance.ts
+++ ui/src/hooks/useAllowance.ts
@@ -2,14 +2,13 @@ import { useReadContract } from "wagmi";
 import useUserWalletConnect from "./useUserWalletConnect";
 import { erc20abi } from "../abis/erc20ABI";
 import { FunctionName } from "../types";
+import { CONTRACT_ADDRESSES } from "../constants";
 
 const useAllowance = () => {
     const { address } = useUserWalletConnect();
-    const USDC_ADDRESS = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
-    const BRIDGE_ADDRESS = "0x092eEA7cE31C187Ff2DC26d0C250B011AEC1a97d";
 
     const wagmiContractConfig = {
-        address: USDC_ADDRESS as `0x${string}`,
+        address: CONTRACT_ADDRESSES.USDC as `0x${string}`,
         abi: erc20abi,
         chainId: 1
     };
@@ -17,7 +16,7 @@ const useAllowance = () => {
     const { data: allowance } = useReadContract({
         ...wagmiContractConfig,
         functionName: FunctionName.Allowance,
-        args: [address as `0x${string}`, BRIDGE_ADDRESS as `0x${string}`]
+        args: [address as `0x${string}`, CONTRACT_ADDRESSES.bridgeAddress as `0x${string}`]
     });
     return {
         allowance
