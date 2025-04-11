6dfd42279ee8cb6a5b3a0c1d5bca3396aa391b5f
tommycp2 <thomasmillerprivate@gmail.com>
2025-04-11 12:16:10 +1000

    fix: update test parameters to use BigInt for consistency

--- pvm/ts/src/commands/mintCommand.test.ts
+++ pvm/ts/src/commands/mintCommand.test.ts
@@ -118,7 +118,7 @@ describe.skip("MintCommand", () => {
                 ethers.ZeroHash,
                 "0x8bf5d2b410baf602fbb1ca59ab16b1772ca0f143950e12a2d4a2ead44ab845fb",
                 1000,
-                1,
+                1n,
                 undefined,
                 "data"
             );
@@ -153,7 +153,7 @@ describe.skip("MintCommand", () => {
                 ethers.ZeroHash,
                 "0x8bf5d2b410baf602fbb1ca59ab16b1772ca0f143950e12a2d4a2ead44ab845fb",
                 1000,
-                1,
+                1n,
                 undefined,
                 "data"
             );
