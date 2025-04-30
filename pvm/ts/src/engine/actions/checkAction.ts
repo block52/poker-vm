0946ca306c7f3e779f9b9a2161d352ec7f88b6ec
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2025-04-30 12:46:35 +1000

    refactor: correct condition for big blind check in CheckAction class

--- pvm/ts/src/engine/actions/checkAction.ts
+++ pvm/ts/src/engine/actions/checkAction.ts
@@ -31,7 +31,7 @@ class CheckAction extends BaseAction implements IAction {
             }
             
             // Big blind CAN check when no one has raised above the big blind
-            if (isBigBlind && largestBet <= this.game.bigBlind) {
+            if (isBigBlind && largestBet === this.game.bigBlind) {
                 return { minAmount: 0n, maxAmount: 0n };
             }
         }
