b42b3e18f23cabd8e062e65ee48da4e8ef455cea
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2025-05-08 12:37:27 +1000

    Refactor CheckAction to improve code clarity and remove redundant checks

--- pvm/ts/src/engine/actions/checkAction.ts
+++ pvm/ts/src/engine/actions/checkAction.ts
@@ -4,7 +4,9 @@ import BaseAction from "./baseAction";
 import { IAction, Range, Turn } from "../types";
 
 class CheckAction extends BaseAction implements IAction {
-    get type(): PlayerActionType { return PlayerActionType.CHECK }
+    get type(): PlayerActionType {
+        return PlayerActionType.CHECK;
+    }
 
     verify(player: Player): Range {
         // Basic validation
@@ -28,15 +30,21 @@ class CheckAction extends BaseAction implements IAction {
             const playerSeat = this.game.getPlayerSeatNumber(player.address);
             const isSmallBlind = playerSeat === this.game.smallBlindPosition;
             const isBigBlind = playerSeat === this.game.bigBlindPosition;
-            
+
             // Small blind CANNOT check in preflop (must call the difference to the big blind)
             if (isSmallBlind && playerBet < this.game.bigBlind) {
                 throw new Error("Small blind must call to match the big blind.");
             }
-            
+
             // Special cast for after blinds and UTG.
             const lastActions = this.game.getActionsForRound(this.game.currentRound);
-            
+
+            // Big blind CAN check when no one has raised above the big blind
+            // In this case, the largest bet is equal to the small blind
+            if (isBigBlind && largestBet === this.game.smallBlind) {
+                return { minAmount: 0n, maxAmount: 0n };
+            }
+
             // Only do bets, calls or raises
             const lastBettingActions = lastActions.filter((action: ActionDTO) => {
                 return action.action === PlayerActionType.BET || action.action === PlayerActionType.CALL || action.action === PlayerActionType.RAISE;
@@ -44,12 +52,6 @@ class CheckAction extends BaseAction implements IAction {
 
             if (!lastBettingActions || lastBettingActions.length === 0) {
                 throw new Error("No previous action to check.");
-            }
-
-            // Big blind CAN check when no one has raised above the big blind
-            // In this case, the largest bet is equal to the small blind
-            if (isBigBlind && largestBet === this.game.smallBlind && !lastActions) {
-                return { minAmount: 0n, maxAmount: 0n };
             }
 
             if (isBigBlind && playerBet < largestBet) {
