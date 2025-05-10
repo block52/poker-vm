684dc2a3a087435e8da26fcb7f95448c7222bbcb
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2025-05-10 15:20:31 +1000

    chore: add redis cli

--- pvm/ts/src/state/index.ts
+++ pvm/ts/src/state/index.ts
@@ -1,3 +1,4 @@
+import Redis from "ioredis";
 import { RedisBlockchainManagement } from "../data/redis";
 import { getAccountManagementInstance } from "./accountManagement";
 import { getContractSchemaManagement } from "./contractSchemaManagement";
@@ -8,7 +9,14 @@ import { getTransactionInstance } from "./transactionManagement";
 export { getAccountManagementInstance, getContractSchemaManagement, getGameManagementInstance, getTransactionInstance };
 
 export const getBlockchainInstance = (): IBlockchainManagement => {
+    // Create a Redis client with non authentication
+    // const redisClient = new Redis({
+    //     host: process.env.REDIS_HOST || "localhost",
+    //     port: parseInt(process.env.REDIS_PORT || "6379", 10),
+    //     password: process.env.REDIS_PASSWORD || undefined,
+    // });
 
-    const instance = new RedisBlockchainManagement();
+    const redisClient = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
+    const instance = new RedisBlockchainManagement(redisClient);
     return instance;
 }
