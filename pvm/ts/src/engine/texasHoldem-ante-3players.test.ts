de5bd4782fa2df99f28ba731475c7003ff0da096
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2025-05-14 14:54:23 +1000

    Add tests for 3 players in random seats and validate legal actions after blinds

--- pvm/ts/src/engine/texasHoldem-ante-3players.test.ts
+++ pvm/ts/src/engine/texasHoldem-ante-3players.test.ts
@@ -53,4 +53,39 @@ describe("Texas Holdem - Ante - 3 Players", () => {
             expect(actions[1].action).toEqual(PlayerActionType.FOLD);
         });
     });
+
+    describe("3 Players in random seats", () => {
+        let game: TexasHoldemGame;
+
+        beforeEach(() => {
+            game = TexasHoldemGame.fromJson(baseGameConfig, gameOptions);
+            game.performAction("0x980b8D8A16f5891F41871d878a479d81Da52334c", NonPlayerActionType.JOIN, 0, ONE_HUNDRED_TOKENS, "5"); // seat 5
+            game.performAction("0x1fa53E96ad33C6Eaeebff8D1d83c95Fcd7ba9dac", NonPlayerActionType.JOIN, 1, ONE_HUNDRED_TOKENS, "2"); // seat 2
+            game.performAction("0x3333333333333333333333333333333333333333", NonPlayerActionType.JOIN, 2, ONE_HUNDRED_TOKENS, "8"); // seat 8
+        });
+
+        it("should have the correct legal options with 3 players after blinds", () => {
+            expect(game.getPlayerCount()).toEqual(3);
+
+            expect(game.getPlayer("0x980b8D8A16f5891F41871d878a479d81Da52334c")).toBeDefined();
+            expect(game.getPlayer("0x1fa53E96ad33C6Eaeebff8D1d83c95Fcd7ba9dac")).toBeDefined();
+            expect(game.getPlayer("0x3333333333333333333333333333333333333333")).toBeDefined();
+
+            expect(game.findNextEmptySeat()).toEqual(4);
+
+            // Perform blinds
+            game.performAction("0x980b8D8A16f5891F41871d878a479d81Da52334c", PlayerActionType.SMALL_BLIND, 3, ONE_TOKEN);
+            game.performAction("0x1fa53E96ad33C6Eaeebff8D1d83c95Fcd7ba9dac", PlayerActionType.BIG_BLIND, 4, TWO_TOKENS);
+
+            // Get round
+            expect(game.currentRound).toEqual(TexasHoldemRound.ANTE);
+
+            // Get legal actions for player 0
+            const actions = game.getLegalActions("0x980b8D8A16f5891F41871d878a479d81Da52334c");
+            expect(actions).toBeDefined();
+            expect(actions.length).toEqual(2);
+            expect(actions[0].action).toEqual(NonPlayerActionType.DEAL);
+            expect(actions[1].action).toEqual(PlayerActionType.FOLD);
+        });
+    });
 });
