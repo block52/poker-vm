9c0b70c43ba5ca472b18c5213dde36b2997f1fe6
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2025-05-16 17:46:22 +1000

    Enhance winner assertions in Texas Holdem Ante test to verify winner details and cards

--- pvm/ts/src/engine/texasHoldem-ante-headsup.test.ts
+++ pvm/ts/src/engine/texasHoldem-ante-headsup.test.ts
@@ -313,7 +313,6 @@ describe("Texas Holdem - Ante - Heads Up", () => {
 
             expect(game.currentRound).toEqual(TexasHoldemRound.END);
 
-
             // Check game states
             const gameState = game.toJson();
             expect(gameState).toBeDefined();
@@ -325,6 +324,14 @@ describe("Texas Holdem - Ante - Heads Up", () => {
             // Check the winner
             expect(gameState.winners).toBeDefined();
             expect(gameState.winners.length).toEqual(1);
+            expect(gameState.winners[0].address).toEqual(SMALL_BLIND_PLAYER);
+            expect(gameState.winners[0].amount).toEqual("400000000000000000");
+            expect(gameState.winners[0].name).toEqual("Flush");
+            expect(gameState.winners[0].description).toEqual("Flush, Ac High");
+            expect(gameState.winners[0].cards).toBeDefined();
+            expect(gameState.winners[0].cards?.length).toEqual(2);
+            expect(gameState.winners[0].cards?.[0]).toEqual("AC");
+            expect(gameState.winners[0].cards?.[1]).toEqual("3C");
 
             // Check hole cards
             expect(gameState.players[0].holeCards).toBeDefined();
