61bca61a40f2dfff48d6a1d863510e7cb3c559a5
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2025-05-16 16:14:54 +1000

    Refactor legal actions test by removing redundant assertions and streamlining the transition from ANTE to PREFLOP.

--- pvm/ts/src/engine/texasHoldem-ante-headsup.test.ts
+++ pvm/ts/src/engine/texasHoldem-ante-headsup.test.ts
@@ -366,19 +366,9 @@ describe("Texas Holdem - Ante - Heads Up", () => {
             actions = game.getLegalActions(SMALL_BLIND_PLAYER);
             expect(actions.length).toEqual(2);
             
-            
-            game.performAction(BIG_BLIND_PLAYER, PlayerActionType.SMALL_BLIND, 0, ONE_TOKEN);
-            expect(game.currentRound).toEqual(TexasHoldemRound.ANTE);
-            expect(game.pot).toEqual(ONE_TOKEN);
-            expect(game.getNextPlayerToAct()?.address).toEqual(SMALL_BLIND_PLAYER);
-
             game.performAction(SMALL_BLIND_PLAYER, PlayerActionType.BIG_BLIND, 1, TWO_TOKENS);
             expect(game.currentRound).toEqual(TexasHoldemRound.ANTE);
             expect(game.pot).toEqual(THREE_TOKENS);
-
-            // Add a DEAL action to advance from ANTE to PREFLOP
-            game.performAction(BIG_BLIND_PLAYER, NonPlayerActionType.DEAL, 2);
-            expect(game.currentRound).toEqual(TexasHoldemRound.PREFLOP);
         });
     });
 });
