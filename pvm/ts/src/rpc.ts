c7ce0c710efa96698a50b33652179a4ddabbc770
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2025-05-19 14:34:58 +1000

    fix: update @bitcoinbrisbane/block52 dependency from 1.0.121 to 1.0.122 refactor: change RPC method from NEW to NEW_HAND and update request parameters

--- pvm/ts/src/rpc.ts
+++ pvm/ts/src/rpc.ts
@@ -345,8 +345,9 @@ export class RPC {
                     break;
                 }
 
-                case RPCMethods.NEW: {
-                    const [to, nonce, index, data] = request.params as RPCRequestParams[RPCMethods.NEW];
+                case RPCMethods.NEW_HAND: {
+                    // This is deprecated, use perform_action instead with action = "new_hand"
+                    const [to, nonce, index, data] = request.params as RPCRequestParams[RPCMethods.NEW_HAND];
                     const command = new NewCommand(to, index, Number(nonce), validatorPrivateKey, data);
                     result = await command.execute();
                     break;
