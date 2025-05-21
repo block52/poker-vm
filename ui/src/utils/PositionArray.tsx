bf4d163393429fe286755d9d08adb454695371e3
Nicholas Makdessi <nicholasmakdessi2002@gmail.com>
2025-05-21 10:55:35 +1000

    Update branch with latest from main

--- ui/src/utils/PositionArray.tsx
+++ ui/src/utils/PositionArray.tsx
@@ -83,6 +83,11 @@ export const playerPosition = {
     ]
 };
 
+export const centerTablePosition = {
+    left: "420px",
+    top: "-320px"
+};
+
 export const vacantPlayerPosition = {
     six: [
         {
