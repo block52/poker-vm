c751c1a09ccdafddedfb458e0b87c0bb0bf3f4b4
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2025-05-22 11:27:56 +1000

    feat: implement showdown actions for players in poker game

--- sdk/test.ts
+++ sdk/test.ts
@@ -1,3 +1,4 @@
+import { NonPlayerActionType } from "./dist/types/game";
 import { PlayerActionType } from "./dist/types/game";
 import { NodeRpcClient } from "./src/client";
 
@@ -11,10 +12,12 @@ const PLAYER_2_PRIVATE_KEY = process.env.PLAYER_2_PRIVATE_KEY;
 const client1 = new NodeRpcClient("http://localhost:3000", PLAYER_1_PRIVATE_KEY || "");
 const client2 = new NodeRpcClient("http://localhost:3000", PLAYER_2_PRIVATE_KEY || "");
 
+// const P1 = "0xccd6e31012fd0ade9beb377c2f20661b832abfe7";
+// const P2 = "0xC84737526E425D7549eF20998Fa992f88EAC2484";
 
 async function main() {
-  const result1 = await client1.playerJoin("0xccd6e31012fd0ade9beb377c2f20661b832abfe7", 1000000000000000000n, 1);
-  const result2 = await client2.playerJoin("0xccd6e31012fd0ade9beb377c2f20661b832abfe7", 1000000000000000000n, 2);
+  let result1 = await client1.playerJoin("0xccd6e31012fd0ade9beb377c2f20661b832abfe7", 1000000000000000000n, 1);
+  let result2 = await client2.playerJoin("0xccd6e31012fd0ade9beb377c2f20661b832abfe7", 1000000000000000000n, 2);
   console.log(result1);
   console.log(result2);
 
@@ -23,6 +26,46 @@ async function main() {
 
   const bbResult = await client2.playerAction("0xccd6e31012fd0ade9beb377c2f20661b832abfe7", PlayerActionType.BIG_BLIND, "20000000000000000");
   console.log(bbResult);
+
+  // deal
+  const dealResult = await client1.deal("0xccd6e31012fd0ade9beb377c2f20661b832abfe7", "2415244220693136377713921289748740858699681395268293", "0xC84737526E425D7549eF20998Fa992f88EAC2484");
+  console.log(dealResult);
+
+  // p1 call
+  const callResult = await client1.playerAction("0xccd6e31012fd0ade9beb377c2f20661b832abfe7", PlayerActionType.CALL, "10000000000000000");
+  console.log(callResult);
+
+  // p2 check
+  const checkResult = await client2.playerAction("0xccd6e31012fd0ade9beb377c2f20661b832abfe7", PlayerActionType.CHECK, "0");
+  console.log(checkResult);
+
+  // flop
+  // p1 check
+  const checkResult2 = await client1.playerAction("0xccd6e31012fd0ade9beb377c2f20661b832abfe7", PlayerActionType.CHECK, "0");
+
+  // p2 check
+  const checkResult3 = await client2.playerAction("0xccd6e31012fd0ade9beb377c2f20661b832abfe7", PlayerActionType.CHECK, "0");
+
+  // turn
+  // p1 check
+  const checkResult4 = await client1.playerAction("0xccd6e31012fd0ade9beb377c2f20661b832abfe7", PlayerActionType.CHECK, "0");
+  // p2 check
+  const checkResult5 = await client2.playerAction("0xccd6e31012fd0ade9beb377c2f20661b832abfe7", PlayerActionType.CHECK, "0");
+
+  // river
+  // p1 check
+  const checkResult6 = await client1.playerAction("0xccd6e31012fd0ade9beb377c2f20661b832abfe7", PlayerActionType.CHECK, "0");
+  // p2 check
+  const checkResult7 = await client2.playerAction("0xccd6e31012fd0ade9beb377c2f20661b832abfe7", PlayerActionType.CHECK, "0");
+
+  // showdown
+  // p1 show
+  const showResult = await client1.playerAction("0xccd6e31012fd0ade9beb377c2f20661b832abfe7", PlayerActionType.SHOW, "0xC84737526E425D7549eF20998Fa992f88EAC2484");
+  console.log(showResult);
+
+  // p2 show
+  const showResult2 = await client2.playerAction("0xccd6e31012fd0ade9beb377c2f20661b832abfe7", PlayerActionType.SHOW, "0xccd6e31012fd0ade9beb377c2f20661b832abfe7");
+  console.log(showResult2);
 }
 
 main().catch((error) => {
