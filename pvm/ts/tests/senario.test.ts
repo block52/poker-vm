1cb82a86b4168c758350e61c673828af0bef8126
Lucas Cullen <bitcoinbrisbane@users.noreply.github.com>
2025-05-23 15:09:06 +1000

    711 mine issue (#748)
    * feat: add expire option to MineCommand constructor and conditionally expire actions
    
    * feat: add test suite for Texas Holdem game engine and include sample game data
    
    * refactor: remove commented-out duplicate transaction handling code and add logging for valid transactions

---
+++ pvm/ts/tests/senario.test.ts
@@ -0,0 +1,28 @@
+import { TexasHoldemRound } from "@bitcoinbrisbane/block52";
+import TexasHoldemGame from "../src/engine/texasHoldem";
+import { fromTestJson } from "../src/engine/testConstants";
+
+
+// This test suite is for the Texas Holdem game engine, specifically for the Ante round in a heads-up scenario.
+describe.skip("Texas Holdem - Data driven", () => {
+    describe("Turn tests", () => {
+
+        const SEAT_1 = "0xE8DE79b707BfB7d8217cF0a494370A9cC251602C";
+        const SEAT_2 = "0x527a896c23D93A5f381C5d1bc14FF8Ee812Ad3dD";
+
+        let game: TexasHoldemGame;
+
+        beforeEach(() => {
+            game = fromTestJson(test_json);
+        });
+
+        it("should have correct legal actions after turn", () => {
+            // Check the current round
+            expect(game.currentRound).toEqual(TexasHoldemRound.TURN);
+
+            // Get legal actions for the next player
+            let actual = game.getLegalActions(SEAT_1);
+            expect(actual).toBeDefined();
+        });
+    });
+});
