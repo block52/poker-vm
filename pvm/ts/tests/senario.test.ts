3944dfb682807bc4fd27c118c05bc38815e7ef4b
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2025-05-23 15:21:36 +1000

    feat: add test case for bug 735 and include test data in the scenario tests

--- pvm/ts/tests/senario.test.ts
+++ pvm/ts/tests/senario.test.ts
@@ -1,10 +1,10 @@
 import { TexasHoldemRound } from "@bitcoinbrisbane/block52";
 import TexasHoldemGame from "../src/engine/texasHoldem";
 import { fromTestJson } from "../src/engine/testConstants";
-import { test_json } from "./senarios/data";
+import { test_json, test_735 } from "./senarios/data";
 
 // This test suite is for the Texas Holdem game engine, specifically for the Ante round in a heads-up scenario.
-describe.skip("Texas Holdem - Data driven", () => {
+describe("Texas Holdem - Data driven", () => {
     describe("Turn tests", () => {
 
         const SEAT_1 = "0xE8DE79b707BfB7d8217cF0a494370A9cC251602C";
@@ -16,9 +16,19 @@ describe.skip("Texas Holdem - Data driven", () => {
             game = fromTestJson(test_json);
         });
 
-        it("should have correct legal actions after turn", () => {
+        it.skip("should have correct legal actions after turn", () => {
             // Check the current round
             expect(game.currentRound).toEqual(TexasHoldemRound.TURN);
+
+            // Get legal actions for the next player
+            let actual = game.getLegalActions(SEAT_1);
+            expect(actual).toBeDefined();
+        });
+
+        it("should test bug 735", () => {
+            game = fromTestJson(test_735)
+            // Check the current round
+            expect(game.currentRound).toEqual(TexasHoldemRound.END);
 
             // Get legal actions for the next player
             let actual = game.getLegalActions(SEAT_1);
