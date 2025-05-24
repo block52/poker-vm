1e5f01b3f91d296264fb3a1b42ae9bb60368bfd0
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2025-05-24 10:29:47 +1000

    fix: update lastActedSeat calculation to use previousActions instead of nextToAct

--- pvm/ts/src/engine/texasHoldem.ts
+++ pvm/ts/src/engine/texasHoldem.ts
@@ -1348,10 +1348,12 @@ class TexasHoldemGame implements IPoker, IUpdate {
         // Reconstruct lastActedSeat from nextToAct
         // If nextToAct is seat N, then lastActedSeat should be N-1 (with wrapping)
         // hack for now need to consider what happens when we get to 9 and how this resets but this works for heads-up.
-        let lastActedSeat = json.nextToAct - 1;
-        if (lastActedSeat < 1) {
-            lastActedSeat = gameOptions.maxPlayers;
-        }
+        // let lastActedSeat = json.nextToAct;
+        // if (lastActedSeat < 1) {
+        //     lastActedSeat = gameOptions.maxPlayers;
+        // }
+
+        let lastActedSeat = json.previousActions[json.previousActions.length - 1]?.seat || 1;
 
         // Create and return new game instance
         return new TexasHoldemGame(
