35093e6d1e51b22783a16ee2ce2aec41afad5e07
Nicholas Makdessi <nicholasmakdessi2002@gmail.com>
2025-05-28 17:18:25 +1000

    Chip animation effect for winning

--- ui/src/components/playPage/Animations/WinAnimation.tsx
+++ ui/src/components/playPage/Animations/WinAnimation.tsx
@@ -6,12 +6,14 @@ import { winAnimationPosition } from "../../../utils/PositionArray";
 import { WinAnimationProps } from "../../../types/index";
 import "./WinAnimation.css";
 
+import DollarChip from "./../../../assets/DollarChip.svg";
+
 const WinAnimation: React.FC<WinAnimationProps> = React.memo(({ index }) => {
   const { id } = useParams<{ id: string }>();
   const { tableSize } = useTableAnimations(id);
   const { winnerInfo } = useWinnerInfo(id);
 
-  // Determine the animation position based on table size
+  // Determine position based on table size
   const position = useMemo(() => {
     switch (tableSize) {
       case 9:
@@ -25,32 +27,39 @@ const WinAnimation: React.FC<WinAnimationProps> = React.memo(({ index }) => {
     }
   }, [tableSize, index]);
 
-  // Check if this seat is among the winners
+  // Only render for the winner
   const isWinner = !!winnerInfo?.some((w) => w.seat === index + 1);
-
-  // Don't render if not a winner or position is unavailable
-  if (!isWinner || !position) {
-    return null;
-  }
+  if (!isWinner || !position) return null;
 
   return (
     <div
       className="win-animation-container"
-      style={{
-        left: position.left,
-        top: position.top,
-      }}
+      style={{ left: position.left, top: position.top }}
     >
+      {/* Ripple rings */}
       {[0, 1, 2, 3].map((i) => (
         <div
-          key={i}
+          key={`ring-${i}`}
           className={`win-animation-ring win-animation-ring-${i}`}
         />
       ))}
+
+      {/* Placeholder bubbles (will become SVG icons) */}
+      <ul className="bubbles">
+        {[0, 1, 2, 3, 4].map((_, i) => (
+          <li
+            key={i}
+            className="bubble"
+            style={{
+              backgroundImage: `url(${DollarChip})`,
+              // you can still override size, timing, etc. in CSS
+            }}
+          />
+        ))}
+      </ul>
     </div>
   );
 });
 
 WinAnimation.displayName = "WinAnimation";
-
 export default WinAnimation;
