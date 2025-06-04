3ae6c5fa5da4b10fade6b5e4d1d042df596ca0f6
Decentralist <thomasmillerprivate@gmail.com>
2025-06-04 13:13:37 +1000

    chore: update @bitcoinbrisbane/block52 dependency to version 1.0.142 and enhance Mempool functionality with real-time updates via WebSocket (#842)

--- explorer/src/App.tsx
+++ explorer/src/App.tsx
@@ -6,26 +6,29 @@ import MempoolPage from "./components/pages/MempoolPage";
 import TransactionsPage from "./components/pages/TransactionsPage";
 import BlockPage from "./components/pages/BlockPage";
 import LatestBlocksPage from "./components/pages/LatestBlocksPage";
+import { MempoolProvider } from "./contexts/MempoolContext";
 
 function App() {
     return (
         <ThemeProvider defaultTheme="dark" storageKey="b52-ui-theme">
-            <Router>
-                <div className="min-h-screen bg-background text-foreground">
-                    <Navbar />
-                    <main className="p-4">
-                        <Routes>
-                            <Route path="/" element={<LatestBlocksPage />} />
-                            <Route path="/blocks" element={<LatestBlocksPage />} />
-                            <Route path="/latestblocks" element={<LatestBlocksPage />} />
-                            <Route path="/block/:index" element={<BlockPage />} />
-                            <Route path="/mempool" element={<MempoolPage />} />
-                            <Route path="/transactions" element={<TransactionsPage />} />
-                        </Routes>
-                    </main>
-                    <Footer />
-                </div>
-            </Router>
+            <MempoolProvider>
+                <Router>
+                    <div className="min-h-screen bg-background text-foreground">
+                        <Navbar />
+                        <main className="p-4">
+                            <Routes>
+                                <Route path="/" element={<LatestBlocksPage />} />
+                                <Route path="/blocks" element={<LatestBlocksPage />} />
+                                <Route path="/latestblocks" element={<LatestBlocksPage />} />
+                                <Route path="/block/:index" element={<BlockPage />} />
+                                <Route path="/mempool" element={<MempoolPage />} />
+                                <Route path="/transactions" element={<TransactionsPage />} />
+                            </Routes>
+                        </main>
+                        <Footer />
+                    </div>
+                </Router>
+            </MempoolProvider>
         </ThemeProvider>
     );
 }
