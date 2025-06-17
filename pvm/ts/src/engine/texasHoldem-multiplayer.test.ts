087ea6eafb045b9b4e72ddde41ab8ab4322e17e2
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2025-06-17 13:51:16 +1000

    fix: remove unused import from Texas Holdem multiplayer test

--- pvm/ts/src/engine/texasHoldem-multiplayer.test.ts
+++ pvm/ts/src/engine/texasHoldem-multiplayer.test.ts
@@ -1,6 +1,6 @@
-import { NonPlayerActionType, PlayerActionType, TexasHoldemRound, TexasHoldemStateDTO } from "@bitcoinbrisbane/block52";
+import { NonPlayerActionType, PlayerActionType, TexasHoldemRound } from "@bitcoinbrisbane/block52";
 import TexasHoldemGame from "./texasHoldem";
-import { baseGameConfig, gameOptions, ONE_HUNDRED_TOKENS, ONE_TOKEN, TWO_TOKENS, mnemonic } from "./testConstants";
+import { baseGameConfig, gameOptions, ONE_HUNDRED_TOKENS, ONE_TOKEN, TWO_TOKENS } from "./testConstants";
 
 // This test suite is for the Texas Holdem game engine, specifically for a multiplayer scenario.
 describe("Texas Holdem - Multiplayer", () => {
