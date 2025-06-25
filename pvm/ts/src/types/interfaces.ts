41f2771dfbcafecbc0e454cc9ea56a93e343ce73
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2025-06-26 08:05:59 +1000

    refactor: implement BetManager for improved bet tracking and integrate into CheckAction and RaiseAction

--- pvm/ts/src/types/interfaces.ts
+++ pvm/ts/src/types/interfaces.ts
@@ -1,4 +1,17 @@
+import { Turn } from "../engine/types";
+
 export interface ICardDeck {
     shuffleDeck(): void;
     drawCard(): string;
+}
+
+export interface IBetManager {
+    add(action: Turn): void;
+    addTurns(turns: Turn[]): void;
+    current(): bigint;
+    delta(): bigint;
+    getTotalBetsForPlayer(playerId: string): bigint;
+    getBets(): Map<string, bigint>;
+    getLargestBet(): bigint;
+    getLastAggressor(start?: number): bigint;
 }
