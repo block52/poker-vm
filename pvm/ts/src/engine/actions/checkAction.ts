1af0ce586cd8a62aef4c41d2742761eb0abc46f6
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2025-06-26 08:33:44 +1000

    refactor: remove unnecessary bet matching logic from CheckAction

--- pvm/ts/src/engine/actions/checkAction.ts
+++ pvm/ts/src/engine/actions/checkAction.ts
@@ -46,61 +46,6 @@ class CheckAction extends BaseAction implements IAction {
         }
 
         throw new Error("Player must match the largest bet to check.");
-
-        // const largestBet = this.getLargestBet(includeBlinds);
-        // const playerBet = this.game.getPlayerTotalBets(player.address, currentRound, includeBlinds);
-
-        // // 2.1 If no bets have been made, player can check
-        // if (largestBet === 0n) {
-        //     return { minAmount: 0n, maxAmount: 0n };
-        // }
-
-        // // // 2.2 If player has already matched the largest bet, they can check
-        // // if (playerBet === largestBet) {
-        // //     return { minAmount: 0n, maxAmount: 0n };
-        // // }
-        
-        // // I think all of this is  unnecessary, but leaving it here for now
-        // // 3. Special case for preflop round
-        // if (currentRound === TexasHoldemRound.PREFLOP) {
-        //     const playerSeat = this.game.getPlayerSeatNumber(player.address);
-        //     const isSmallBlind = playerSeat === this.game.smallBlindPosition;
-        //     const isBigBlind = playerSeat === this.game.bigBlindPosition;
-
-        //     // Small blind CANNOT check in preflop (must call the difference to the big blind)
-        //     if (isSmallBlind && playerBet < this.game.bigBlind) {
-        //         throw new Error("Small blind must call to match the big blind.");
-        //     }
-
-        //     // Special cast for after blinds and UTG.
-        //     const lastActions = this.game.getActionsForRound(this.game.currentRound);
-
-        //     // Big blind CAN check when no one has raised above the big blind
-        //     // In this case, the largest bet is equal to the small blind
-        //     if (isBigBlind && largestBet === this.game.smallBlind) {
-        //         return { minAmount: 0n, maxAmount: 0n };
-        //     }
-
-        //     // Only do bets, calls or raises
-        //     const lastBettingActions = lastActions.filter((action: Turn) => {
-        //         return action.action === PlayerActionType.BET || action.action === PlayerActionType.CALL || action.action === PlayerActionType.RAISE;
-        //     });
-
-        //     if (!lastBettingActions || lastBettingActions.length === 0) {
-        //         throw new Error("No previous action to check.");
-        //     }
-
-        //     if (isBigBlind && playerBet < largestBet) {
-        //         throw new Error("Big blind must call to match the big blind.");
-        //     }
-        // }
-
-        // // 4. General case: Can only check if player has matched the largest bet
-        // if (playerBet < largestBet) {
-        //     throw new Error("Player must match the largest bet to check.");
-        // }
-
-        return { minAmount: 0n, maxAmount: 0n };
     }
 
     verify_old(player: Player): Range {
