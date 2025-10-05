f871cdea612aae4b8885d8f07bb85a3dd47dd203
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2025-10-05 16:30:12 +1000

    Adds Cosmos Bridge contract
    This commit introduces the CosmosBridge contract, which facilitates the transfer of tokens between Ethereum and Cosmos.
    
    The contract includes functionality for:
    - Depositing tokens from Ethereum to Cosmos
    - Withdrawing tokens from Cosmos to Ethereum, secured by validator signatures.
    - Swapping tokens via Uniswap V3 if the deposited token is not the underlying token.
    - Emergency withdrawal functionality for the contract owner.
    
    Removes Sepolia network configurations and updates base network.

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,6 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "@nomicfoundation/hardhat-verify"; 
+import "@nomicfoundation/hardhat-verify";
 
 import dotenv from "dotenv";
 dotenv.config();
@@ -17,20 +17,10 @@ const config: HardhatUserConfig = {
             //     url: `${process.env.RPC_URL}`
             // }
         },
-        sepolia: {
-            url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
-            accounts: PK ? [PK] : [],
-            chainId: 11155111
-        },
         base: {
-			accounts: PK ? [PK] : [],
-			chainId: 8453,
-			url: "https://mainnet.base.org",
-		},
-        baseSepolia: {
             accounts: PK ? [PK] : [],
-            chainId: 84532,
-            url: "https://sepolia.base.org",
+            chainId: 8453,
+            url: "https://mainnet.base.org",
         },
         mainnet: {
             chainId: 1,
@@ -50,9 +40,7 @@ const config: HardhatUserConfig = {
     },
     etherscan: {
         apiKey: {
-            sepolia: process.env.ETHERSCAN_API_KEY || "",
             base: process.env.ETHERSCAN_API_KEY || "",
-            baseSepolia: process.env.ETHERSCAN_API_KEY || "",
             mainnet: process.env.ETHERSCAN_API_KEY || ""
         },
         customChains: [
@@ -62,14 +50,6 @@ const config: HardhatUserConfig = {
                 urls: {
                     apiURL: "https://api.basescan.org/api",
                     browserURL: "https://basescan.org"
-                }
-            },
-            {
-                network: "baseSepolia",
-                chainId: 84532,
-                urls: {
-                    apiURL: "https://api-sepolia.basescan.org/api",
-                    browserURL: "https://sepolia.basescan.org"
                 }
             }
         ]
