ed6f8541ad9e1c35bde45e5b38c6cf4463d7c501
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2025-10-08 08:06:49 +1000

    refactor: remove commented-out code and clean up PerformActionCommand

--- pvm/ts/src/commands/cosmos/performActionCommand.ts
+++ pvm/ts/src/commands/cosmos/performActionCommand.ts
@@ -36,56 +36,7 @@ export class PerformActionCommand implements ICommand<TexasHoldemStateDTO> {
             }
         }
 
-        // const encodedData = params.toString();
-        // const tx: Transaction = await Transaction.create(
-        //     this.to, // game receives funds (to)
-        //     this.from, // player sends funds (from)
-        //     this.value, // no value for game actions
-        //     nonce,
-        //     this.privateKey,
-        //     encodedData
-        // );
-
-
-        // // Get mempool transactions for the game
-        // const mempoolTransactions: Transaction[] = this.mempool.findAll(tx => tx.to === this.to && tx.data !== undefined && tx.data !== null && tx.data !== "");
-        // console.log(`Found ${mempoolTransactions.length} mempool transactions`);
-
-        // // Sort transactions by index
-        // const orderedTransactions = mempoolTransactions.map(tx => toOrderedTransaction(tx)).sort((a, b) => a.index - b.index);
-        // if (!this.addToMempool) {
-        //     // If we're not adding to the mempool, we still need to process the transaction
-        //     orderedTransactions.push(toOrderedTransaction(tx));
-        //     console.log(`Added current transaction to ordered transactions: ${tx.hash}`);
-        // }
-
-        // const gameOptions = await this.gameManagement.getGameOptions(gameState.address);
-        // const game: TexasHoldemGame = TexasHoldemGame.fromJson(gameState.state, gameOptions);
-
-        // orderedTransactions.forEach(tx => {
-        //     try {
-        //         console.log(`Processing ${tx.type} action from ${tx.from} with value ${tx.value}, index ${tx.index}, and data ${tx.data}`);
-        //         game.performAction(tx.from, tx.type, tx.index, tx.value, tx.data);
-
-        //     } catch (error) {
-        //         console.warn(`Error processing transaction ${tx.index} from ${tx.from}: ${(error as Error).message}`);
-        //         // Continue with other transactions, don't let this error propagate up
-        //     }
-        // });
-
-
         this.texasHoldemGame.performAction(this.from, this.action, this.index, this.value, this.data);
-
-        // const txResponse: TransactionResponse = {
-        //     nonce: tx.nonce.toString(),
-        //     to: tx.to,
-        //     from: tx.from,
-        //     value: this.value.toString(),
-        //     hash: tx.hash,
-        //     signature: tx.signature,
-        //     timestamp: tx.timestamp.toString(),
-        //     data: encodedData
-        // };
 
         const updatedGameState: TexasHoldemStateDTO = this.texasHoldemGame.toJson();
         return updatedGameState;
